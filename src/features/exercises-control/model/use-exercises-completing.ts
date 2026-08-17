fb5ff198a8f024d614f1bbd122c02cc74e5db8a1
Bogdan <4634815@gmail.com>
2026-08-17 15:44:52 +0300

    Adding completing exercises UI and server function, create exercises button

---
+++ src/features/exercises-control/model/use-exercises-completing.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "convex/react";
+import { api } from "@/../convex/_generated/api";
+import { Id } from "@/../convex/_generated/dataModel";
+
+export const useExercisesCompleting = (
+  _id: Id<"exercises">,
+  isCompleted: boolean,
+) => {
+  const completingExercisesMutation = useMutation(
+    api.functions.exercises.completeExercise,
+  );
+
+  const handleCompleting = async () => {
+    if (!_id) return;
+    if (isCompleted === true) {
+      return { success: false, message: "Ошибка задача уже выполнена" };
+    }
+
+    try {
+      const res = await completingExercisesMutation({ _id, isCompleted });
+      return res;
+    } catch (error) {
+      console.error("Ошибка при выполнении задачи:", error);
+      return { success: false, message: "Ошибка выполнения запроса" };
+    }
+  };
+
+  return { handleCompleting };
+};
--- src/widgets/exercises-list/ui/exercises-list-skeleton.tsx
+++ src/widgets/exercises-list/ui/exercises-list-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/shared";
 export const ExercisesListSkeleton = () => {
   return (
     <div className="flex flex-col gap-2">
-      <Skeleton className="px-3 py-2 w-full h-[28px] rounded-xl" />
+      <Skeleton className="px-3 mt-1.5 py-2 w-full h-[22px] rounded-xl" />
       <div className="flex flex-col py-1 pr-1 gap-3">
         {Array.from({ length: 6 }).map((_, index) => (
           <Skeleton key={index} className="w-full h-[68px] rounded-xl" />
